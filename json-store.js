1532073f560a44007d42f535be6097fe83e4df56
Sri <2161280+keepRunning@users.noreply.github.com>
2021-11-18 08:52:08 +0530

    Initial commit

---
+++ json-store.js
@@ -0,0 +1,83 @@
+const https = require('https');
+
+function update(dataObj) {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(dataObj);
+
+        const options = {
+            protocol: 'https:',
+            hostname: 'api.jsonbin.io',
+            port: 443,
+            path: '/v3/b/61926c5501558c731cc2bf47',
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Master-Key': process.env.JSONBIN_MASTERKEY,
+                'X-Bin-Versioning': false
+            }
+        };
+
+        const req = https.request(options, res => {
+
+            let responseData = ''
+
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(responseData);
+            })
+        });
+
+        req.on("error", (error) => {
+            console.log("Error: " + error.message);
+            rej();
+        });
+
+        req.write(data)
+        req.end();
+    })
+
+}
+
+function fetch() {
+    return new Promise((resolve, reject) => {
+        const options = {
+            protocol: 'https:',
+            hostname: 'api.jsonbin.io',
+            port: 443,
+            path: '/v3/b/61926c5501558c731cc2bf47',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Master-Key': process.env.JSONBIN_MASTERKEY,
+                'X-Bin-Versioning': false
+            }
+        };
+
+        const req = https.request(options, res => {
+
+            let responseData = ''
+
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+
+            res.on('end', () => {
+                let d2 = JSON.parse(responseData);
+                resolve(d2.record);
+            })
+        });
+
+        req.on("error", (error) => {
+            console.log("Error: " + error.message);
+            reject();
+        });
+
+        req.end();
+    })
+
+}
+
+module.exports = { update, fetch }
